8ee3c7a56786998683a999e29226e8b2c40ae043
rafa-mesquita <rafamesquita2007@hotmail.com>
2026-04-19 19:32:44 -0300

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/halloween-dawn-anger/',
   server: {
     host: true,
     allowedHosts: ['.trycloudflare.com', '.loca.lt', '.ngrok-free.app', '.ngrok.io'],
